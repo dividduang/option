40a0d59860547a0cc5c9594bc572f433a39b94e9
dengjingren <dengjingren@cn.wilmar-intl.com>
2025-06-28 15:47:12 +0800

    feat 适配新版fba规则

--- option.sql
+++ option.sql
@@ -35,5 +35,5 @@ create table sys_api_config
 )
     comment 'API Key配置表';
 
-insert into fba.sys_menu (id, title, name, path, sort, icon, type, component, perms, status, display, cache, link, remark, parent_id, created_time, updated_time)
-values  (23, '配置下发', 'Option', 'option', 999, 'eos-icons:admin', 0, null, null, 1, 1, 1, null, null, null, '2023-07-27 19:23:00', null);
+INSERT INTO fba.sys_menu (title, name, path, sort, icon, type, component, perms, status, display, cache, link, remark, parent_id, created_time, updated_time)
+VALUES ('配置下发', 'Option', 'option', 7, 'eos-icons:admin', 0, null, null, 1, 1, 1, '', null, null, now(), null);
